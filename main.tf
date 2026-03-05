63560022e6a0667ee90fb3c5092e9e0b34172613
Pawan Kumar123 <kumaradi9793368884@gmail.com>
2026-03-05 08:22:18 +0000

    terraform fix

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "jenkin" {
-  ami           = "ami-0b6c6ebed2801a5cb" # ami-id
+  ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.micro"
-  key_name      = "devops1.pem"
+  key_name      = "devops1"
 
   user_data = <<-EOF
-              #!/bin/bash
-              yum install nginx -y
-              systemctl start nginx
-              systemctl enable nginx
-              echo "Hello from Terraform" > /usr/share/nginx/html/index.html
-              EOF
+#!/bin/bash
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+echo "Hello from Terraform" > /usr/share/nginx/html/index.html
+EOF
 
   tags = {
     Name = "Jenkins-Terraform-Server"
   }
-}i
+}
